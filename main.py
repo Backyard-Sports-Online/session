c35907806433efaac08ead048a8f537ab34f1c34
Little Cat <toontownlittlecat@gmail.com>
2021-10-08 03:50:31 -0300

    main: Add traffic relaying

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ session_counter = 0
 # {sessionId: peer}
 sessions = {}
 
+tunnel_counter = 0
+# {sessionId: peer}
+tunnels = {}
+
+# {peer: peer}
+relays = {}
+
 debug = 0
 
 def print_debug(*args):
     if debug:
-        print(*args)
+        print(*args, flush=True)
 
 def send(peer, data):
     print_debug('sending:', data)
@@ -30,8 +37,21 @@ while True:
             if peer == event.peer:
                 del sessions[session]
                 break
+
+        if event.peer in relays:
+            # Disconnect their relaying peer.
+            dest_peer = relays[event.peer]
+            dest_peer.disconnect()
+            del relays[event.peer]
+            if dest_peer in relays:
+                del relays[dest_peer]
     elif event.type == enet.EVENT_TYPE_RECEIVE:
         print_debug("%s: IN:  %r" % (event.peer.address, event.packet.data))
+
+        if event.peer in relays:
+            # Relay this packet to their peer
+            relays[event.peer].send(0, event.packet)
+            continue
         try:
             data = json.loads(event.packet.data)
         except:
@@ -58,7 +78,7 @@ while True:
                 continue
             host_peer = sessions.get(session)
             if not host_peer:
-                print_debug(f'{event.peer.address} attempted to join session {session}, but not on sessions!')
+                print(f'{event.peer.address} attempted to join session {session}, but not on sessions!', flush=True)
                 continue
             send(host_peer, {
                 'cmd': 'peer_join',
@@ -72,3 +92,26 @@ while True:
 
                 'host_port': event.peer.address.port
             })
+        elif command == 'host_relay':
+            tunnel_counter += 1
+            tunnels[tunnel_counter] = event.peer
+
+            send(event.peer, {
+                'cmd': 'relay_resp',
+                'tunnel': tunnel_counter,
+            })
+        elif command == 'join_relay':
+            tunnel = data.get('tunnel')
+            if not tunnel:
+                print_debug('join_relay is missing tunnel!')
+                continue
+            host_peer = tunnels.get(tunnel)
+            if not host_peer:
+                print(f'{event.peer.address} attempted to join tunnel {tunnel}, but not on tunnels!', flush=True)
+                continue
+
+            relays[host_peer] = event.peer
+            relays[event.peer] = host_peer
+
+            send(host_peer, {'cmd': 'relay_join'})
+            send(event.peer, {'cmd': 'relay_join'})
